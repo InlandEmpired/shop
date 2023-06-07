d139cd7ee4554a92290b45331377f51467474590
BragaruAndrei <notbragaru@gmail.com>
2023-06-07 17:49:27 +0300

    fix

--- pages/thanks.js
+++ pages/thanks.js
@@ -4,9 +4,10 @@ export default function Thanks(){
     return <div className="grid h-screen place-items-center">
         <div className="grid gap-4 grid-cols-1">
         <h1 className="text-center text-2xl font-bold text-emerald-500 ">Multumesc pentru cumparare</h1>
-        <Link href="/" className="bg-emerald-500 px-5 py-2 rounded-xl
+        <Link href="/">
+            <button  className="bg-emerald-500 px-5 py-2 rounded-xl
         font-bold text-white w-full my-4 shadow-emerald-300 
-        shadow-lg text-center">Reveniti acasa</Link>
+        shadow-lg text-center">Reveniti acasa</button></Link>
         </div>
     </div>
 }
