82e0c72fad34b064e65433658164c65ba990e774
Inland Empire <bragaru.a2@gmail.com>
2024-09-28 10:27:14 +0300

    fix

--- pages/thanks.js
+++ pages/thanks.js
@@ -3,11 +3,11 @@ import Link from "next/link";
 export default function Thanks(){
     return <div className="grid h-screen place-items-center">
         <div className="grid gap-4 grid-cols-1">
-        <h1 className="text-center text-2xl font-bold text-emerald-500 ">Multumesc pentru cumparare</h1>
+        <h1 className="text-center text-2xl font-bold text-emerald-500 ">Благодарим за приобретение!</h1>
         <Link href="/">
             <button  className="bg-emerald-500 px-5 py-2 rounded-xl
         font-bold text-white w-full my-4 shadow-emerald-300 
-        shadow-lg text-center">Reveniti acasa</button></Link>
+        shadow-lg text-center">Вернуться на главную страницу</button></Link>
         </div>
     </div>
 }
